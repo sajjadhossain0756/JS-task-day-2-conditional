111c8ca9c9ee9696f0b914498ec42515e7ae604f
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-29 22:10:53 +0600

    task one & two complete

---
+++ index.js
@@ -0,0 +1,23 @@
+// task-1
+var burgerPrice = 600;
+
+if (burgerPrice > 500 ){
+    console.log("You will get cock free");
+}else{
+    console.log("You have to buy cock by 30 taka");
+}
+// task-2
+var bmi = 20;
+
+if(bmi < 18.5){
+    console.log("You are underweight");
+}
+else if(bmi >= 18.5 && bmi <= 24.9){
+    console.log("You are normal");
+}
+else if(bmi >= 25 && bmi <= 29){
+    console.log("You are overweight");
+}
+else{
+    console.log("You are obese");
+}
