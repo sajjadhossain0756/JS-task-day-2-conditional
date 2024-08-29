e7f0db4b196e4a4da09bc1e46eb23680048f7795
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-29 23:10:07 +0600

    task-3 complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// task-1
+// task-1 start here
 var burgerPrice = 600;
 
 if (burgerPrice > 500 ){
@@ -6,7 +6,7 @@ if (burgerPrice > 500 ){
 }else{
     console.log("You have to buy cock by 30 taka");
 }
-// task-2
+// task-2 start here
 var bmi = 20;
 
 if(bmi < 18.5){
@@ -20,4 +20,26 @@ else if(bmi >= 25 && bmi <= 29){
 }
 else{
     console.log("You are obese");
+}
+// task-3 start here
+
+var studentMarks = 80;
+
+if(studentMarks >= 0 && studentMarks <= 59){
+    console.log("You are got 'F' Grade");
+}
+else if(studentMarks >= 60 && studentMarks <= 69){
+    console.log("You are got 'D' Grade");   
+}
+else if(studentMarks >= 70 && studentMarks <= 79){
+    console.log("You are got 'C' Grade");   
+}
+else if(studentMarks >= 80 && studentMarks <= 89){
+    console.log("You are got 'B' Grade");   
+}
+else if(studentMarks >= 90 && studentMarks <= 100){
+    console.log("You are got 'A' Grade");   
+}
+else{
+    console.log("Not a valid Marks")
 }
